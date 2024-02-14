9644d5987249113a8874e4b1298a843b1a758da7
IaDrake20 <ios.drake05@gmail.com>
2024-02-13 20:47:08 -0500

    decided to add an Order class
    figured the gob class wasn't the right place to keep track of everything an order encompasses

--- BlobsOfGobs/Gob.cs
+++ BlobsOfGobs/Gob.cs
@@ -4,11 +4,18 @@ namespace BlobsOfGobs
 {
     public class Gob
     {
-        private string name;
+        private string name = 0;
         public string Name
         {
             get { return this.name; }
             set { this.name = value;}
+        }
+
+        private int numberBought = 0;
+        public int NumberBought
+        {
+            get { return this.numberBought; }
+            set { this.numberBought = value; }
         }
     }
 }
---
+++ BlobsOfGobs/Order.cs
@@ -0,0 +1,32 @@
+﻿namespace BlobsOfGobs
+{
+    /**
+     * IAN: an order should contain a guid, list of gobs bought, bool tracking whether the order is paid for
+     * 
+     * 
+     **/
+    public class Order
+    {
+        //IAN: Order ID
+        private Guid id = Guid.NewGuid();
+        public Guid Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private List<Gob> orders = new List<Gob>();
+        public List<Gob> Orders
+        {
+            get { return orders; }
+            set { orders = value; }
+        }
+
+        private bool isPaidFor = false;
+        public bool IsPaidFor
+        {
+            get { return isPaidFor; }
+            set { isPaidFor = value; }
+        }
+    }
+}
