b34ba4d09c273eb461b56ec41bedb4125379260d
oravix27 <128840829+oravix27@users.noreply.github.com>
2024-02-17 22:59:49 -0500

    Created Base GUI
    In case things go wrong, here's this to go back to.

---
+++ GobGUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GobGUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
