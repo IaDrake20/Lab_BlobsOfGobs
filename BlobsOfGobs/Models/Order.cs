78364ad57a2fddb6d456c6960314ac49abcbfe7e
IaDrake20 <ios.drake05@gmail.com>
2024-02-18 19:23:39 -0500

    Revert "added more controllers"
    This reverts commit 26ebd045aa135bd024a031c7f6c377f82794260b.

---
+++ BlobsOfGobs/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_BlobsOfGobs.Models;
+
+public partial class Order
+{
+    public string Order1 { get; set; } = null!;
+
+    public string? Name { get; set; }
+}
