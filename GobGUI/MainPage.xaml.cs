a2790b78a6ce68247063cc7eaedef78aecb4dc0a
oravix27 <128840829+oravix27@users.noreply.github.com>
2024-02-19 02:18:21 -0500

    Working first two pages

--- GobGUI/MainPage.xaml.cs
+++ GobGUI/MainPage.xaml.cs
@@ -16,48 +16,63 @@ namespace GobGUI
             InitializeComponent();
         }
 
-       
-        async private Task<List<Order>> GetPeopleAsync()
+        private void ShowOrdersBtn_Clicked(object sender, EventArgs e)
         {
-            string apiUrl = "https://localhost:7005/api/Orders";
-            List<Order> orders = null;
-            using (HttpClient client = new HttpClient()) 
-            { 
-                try
-                {
-                    HttpResponseMessage response = await client.GetAsync(apiUrl);
 
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        string jsonString = await response.Content.ReadAsStringAsync();
-                        orders = JsonConvert.DeserializeObject<List<Order>>(jsonString);
-                    }
-                    else
-                    {
-                        Debug.WriteLine("API request failed with status code:" + response.StatusCode);
-                        return null;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Error: " + ex.Message);
-                    return null;
-                }
-            }
-            return orders;
         }
 
-        async private void GetDataBtn_Clicked(object sender, EventArgs e)
+        private void EditOrderBtn_Clicked(object sender, EventArgs e)
         {
-            Debug.WriteLine("GetDataBtn_Clicked");
-            var orders = await GetPeopleAsync();
 
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                ordersListView.ItemsSource = orders;
-            });
         }
+
+        private void StartOrderBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //    async private Task<List<Order>> GetPeopleAsync()
+        //    {
+        //        string apiUrl = "https://localhost:7005/api/Orders";
+        //        List<Order> orders = null;
+        //        using (HttpClient client = new HttpClient()) 
+        //        { 
+        //            try
+        //            {
+        //                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+
+        //                if (response.IsSuccessStatusCode)
+        //                {
+        //                    string jsonString = await response.Content.ReadAsStringAsync();
+        //                    orders = JsonConvert.DeserializeObject<List<Order>>(jsonString);
+        //                }
+        //                else
+        //                {
+        //                    Debug.WriteLine("API request failed with status code:" + response.StatusCode);
+        //                    return null;
+        //                }
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                Console.WriteLine("Error: " + ex.Message);
+        //                return null;
+        //            }
+        //        }
+        //        return orders;
+        //    }
+
+        //    async private void GetDataBtn_Clicked(object sender, EventArgs e)
+        //    {
+        //        Debug.WriteLine("GetDataBtn_Clicked");
+        //        var orders = await GetPeopleAsync();
+
+        //        MainThread.BeginInvokeOnMainThread(() =>
+        //        {
+        //            ordersListView.ItemsSource = orders;
+        //        });
+        //    }
     }
 
 }
