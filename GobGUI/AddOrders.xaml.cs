8e18e6b36633139818cebab645e57d83186b9b13
oravix27 <128840829+oravix27@users.noreply.github.com>
2024-02-19 04:20:09 -0500

    Copy Button Added

--- GobGUI/AddOrders.xaml.cs
+++ GobGUI/AddOrders.xaml.cs
@@ -1,3 +1,4 @@
+
 namespace GobGUI;
 
 public partial class NewPage1 : ContentPage
@@ -9,9 +10,22 @@ public partial class NewPage1 : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
-		//temporary way to show different orders
-		int orderNum = 1;
+        //temporary way to show different orders
+        orderNum++;		
 		lblSubmit.Text = "Order #" + orderNum + " was successfully submitted.";
-		orderNum++;
+		
+        btnCopy.IsVisible = true;
     }
+
+    private async void Button_Clicked2(object sender, EventArgs e)
+    {
+        String valueToCopy = "" + orderNum;
+
+        await Clipboard.SetTextAsync(valueToCopy);
+
+        // Optionally, show a confirmation to the user
+        lblSubmit.Text = "Order #" + orderNum + " was copied to the clipboard.";
+    }
+
+    public int orderNum = 0;
 }
