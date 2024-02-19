064a87ab7d68145507f75e7cb656a298f4aba544
Mscar1235 <129341137+Mscar1235@users.noreply.github.com>
2024-02-18 20:26:03 -0500

    OrderGob class

---
+++ BlobsOfGobs/OrderGob.cs
@@ -0,0 +1,28 @@
+﻿namespace API_BlobsOfGobs
+{
+    public class OrderGob
+    {
+        private Guid customerid;
+        private Guid flavorid;
+        private Guid ordergobid;
+
+        public Guid OrderGobID
+        {
+            get { return ordergobid; }
+            set { ordergobid = value; }
+        }
+
+        public Guid FlavorID { 
+
+            get { return flavorid; }
+            set { flavorid = value; }
+        }
+
+        public Guid CustomerID {
+
+            get { return customerid; } 
+            set {  customerid = value; } 
+        }
+
+    }
+}
