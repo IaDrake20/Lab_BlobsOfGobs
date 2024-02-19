d24a414ad35f79a96078d9ee2a12da0b87db3982
oravix27 <128840829+oravix27@users.noreply.github.com>
2024-02-19 03:51:35 -0500

    Order confirmation

--- GobGUI/AddOrders.xaml.cs
+++ GobGUI/AddOrders.xaml.cs
@@ -6,4 +6,12 @@ public partial class NewPage1 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		//temporary way to show different orders
+		int orderNum = 1;
+		lblSubmit.Text = "Order #" + orderNum + " was successfully submitted.";
+		orderNum++;
+    }
 }
