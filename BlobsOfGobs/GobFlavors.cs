855d9d3bc6143997fc6ce43d8f8969a18887b1ff
Mscar1235 <129341137+Mscar1235@users.noreply.github.com>
2024-02-18 20:11:36 -0500

    GobFlavors api fix

--- BlobsOfGobs/GobFlavors.cs
+++ BlobsOfGobs/GobFlavors.cs
@@ -3,31 +3,23 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace API_BlobsOfGobs
 {
-    public class Gob
+    public class GobFlavors
     {
         //IAN: I added this to set up the controller, for the future I'm not sure if every single gob needs a GUID
         private Guid id = Guid.NewGuid();
 
         [Key]
-        public Guid Id
+        public Guid FlavorID
         {
             get { return id; }
             set { id = value; }
         }
 
         private string name = "";
-        public string Name
+        public string FlavorName
         {
             get { return name; }
             set { name = value; }
-        }
-
-        private int numberBought = 0;
-
-        public int NumberBought
-        {
-            get { return numberBought; }
-            set { numberBought = value; }
         }
     }
 }
